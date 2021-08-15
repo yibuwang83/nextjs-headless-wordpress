b106f2dca0f0c1466d0c50f32a79377ef5352aa5
name <email>
2021-08-16 01:20:06 +0800

    update code

--- web/src/apollo/client.js
+++ web/src/apollo/client.js
@@ -1,6 +1,32 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache(),
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
+
+const cache = new InMemoryCache({
+  resultCaching: false,
 });
+
+const link = createHttpLink({
+  uri: `${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/graphql`,
+});
+console.log(
+  '🚀 ~ file: client.js ~ line 21 ~ ${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/graphql',
+  process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL
+);
+
+const client = new ApolloClient({
+  link,
+  cache,
+  defaultOptions,
+});
+
+export default client;
---
+++ web/src/queries/fragment/menus.js
@@ -0,0 +1,12 @@
+
+
+const MenuFragment = `
+  fragment MenuFragment on MenuItem {
+      id
+      label
+      url
+      path
+  }
+`
+
+export default MenuFragment;
